c85c9e77c25e622f0dbe315075d82321df0caeda
Maria Eugênia <beds03@gmail.com>
2019-10-20 17:31:48 -0300

    created new project

---
+++ SeriesDB/SeriesDB/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SeriesDB
+//
+//  Created by Maria Eugênia Pereira Teixeira on 20/10/19.
+//  Copyright © 2019 Maria Eugênia Pereira Teixeira. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
